dacd7f1011a1e0a85ac8310666e6a40db4c1dcb4
Yong Jian Ming <jianming1993@gmail.com>
2019-10-25 17:47:41 +0800

    Update

--- hc_sr04.c
+++ hc_sr04.c
@@ -1,36 +1,51 @@
 #include "hc_sr04.h"
 
-volatile uint16_t captureTime[2];
-volatile uint8_t captureIndex = 0;
-volatile uint16_t captureDistance = 0;
+#define UINT16_MAX_VALUE (1 << 16) - 1;
 
-void setup_ultrasonic_pins(void)
+volatile uint8_t capture_index = 0;
+volatile uint16_t capture_offset = 0;
+volatile uint16_t capture_time[2];
+volatile uint16_t capture_distance = 0;
+volatile uint16_t capture_threshold = 400;
+
+void setup_ultrasonic_pins(const hc_sr04_config *config);
+void setup_ultrasonic_capture_timer(void);
+
+void initialize_ultrasonic_sensor(const hc_sr04_config *config, const uint16_t threshold)
 {
-    // This set the trigger pin as a GPIO.
-    TRIGGER_PORT->SEL0 &= ~TRIGGER_PIN;
-    TRIGGER_PORT->SEL1 &= ~TRIGGER_PIN;
+    capture_threshold = threshold;
 
-    // This set the trigger pin as output direction.
-    TRIGGER_PORT->DIR |= TRIGGER_PIN;
-
-    // This set the trigger pin to 0 to prevent trigger.
-    TRIGGER_PORT->OUT &= ~TRIGGER_PIN;
-
-    // This set the echo pin to Timer A capture input.
-    ECHO_PORT->SEL0 |= ECHO_PIN;
-    ECHO_PORT->SEL1 &= ~ECHO_PIN;
-
-    // This set the echo pin to TAx.CCIxA.
-    ECHO_PORT->DIR &= ~ECHO_PIN;
+    setup_ultrasonic_pins(config);
+    setup_ultrasonic_capture_timer();
 }
 
-void setup_ultrasonic_capture_timers(void)
+void setup_ultrasonic_pins(const hc_sr04_config *config)
+{
+    // This set the trigger pin as a GPIO.
+    *config->trigger_port_sel0 &= ~config->trigger_port_pin;
+    *config->trigger_port_sel1 &= ~config->trigger_port_pin;
+
+    // This set the trigger pin as output direction.
+    *config->trigger_port_dir |= config->trigger_port_pin;
+
+    // This set the trigger pin to 0 to prevent trigger.
+    *config->trigger_port_out &= ~config->trigger_port_pin;
+
+    // This set the echo pin to Timer A capture input.
+    *config->echo_port_sel0 |= config->echo_port_pin;
+    *config->echo_port_sel1 &= ~config->echo_port_pin;
+
+    // This set the echo pin to TAx.CCIxA.
+    *config->echo_port_dir &= ~config->echo_port_pin;
+}
+
+void setup_ultrasonic_capture_timer(void)
 {
     /*
      * Timer A0 Control Register
-     * SMCLK | Divider 1 | Continuous mode | Clear
+     * SMCLK | Divider 1 | Continuous mode | Clear | Enable overflow interrupt
      */
-    TIMER_A0->CTL = TASSEL_2 | ID_0 | MC_2 | TACLR;
+    TIMER_A0->CTL |= TASSEL_2 | ID_0 | MC_2 | TACLR | TAIE;
 
     /*
      * Timer A0 Capture Control Register
@@ -40,7 +55,7 @@ void setup_ultrasonic_capture_timers(void)
      * CAP: Capture mode
      * CCIE: Capture interrupt
      */
-    TIMER_A0->CCTL[1] = CM_3 | CCIS_0 | SCS | CAP | CCIE;
+    TIMER_A0->CCTL[1] |= CM_3 | CCIS_0 | SCS | CAP | CCIE;
 
     NVIC->ISER[0] |= 1 << ((TA0_N_IRQn) & 31);
 }
@@ -57,9 +72,22 @@ void trigger_ultrasonic_sensor(void)
 
     // Reset timer to zero.
     TIMER_A0->CTL |= TACLR;
+
+    capture_index = 0;
+    capture_offset = 0;
 }
 
-void delay_us(int n)
+uint8_t is_object_found(void)
+{
+    return capture_distance <= capture_threshold;
+}
+
+uint16_t get_object_distance(void)
+{
+    return capture_distance;
+}
+
+void delay_us(const uint32_t amount)
 {
     // Set Timer32 to 1 microseconds.
     TIMER32_1->LOAD = SystemCoreClock / 1000000;
@@ -67,9 +95,9 @@ void delay_us(int n)
     // Set Timer32 to periodic warping mode.
     TIMER32_1->CONTROL = 0xC2;
 
-    int i = n;
+    volatile int count = amount;
 
-    for (i = n; i > 0; i--) {
+    for (count = amount; count > 0; count--) {
         /* wait until the RAW_IFG is set */
         while((TIMER32_1->RIS & 1) == 0);
 
@@ -80,15 +108,36 @@ void delay_us(int n)
 
 void TA0_N_IRQHandler(void)
 {
-    if (TIMER_A0->CCTL[1] & TIMER_A_CCTLN_CCIFG) {
-        captureTime[captureIndex++] = TIMER_A0->CCR[1];
-
-        if (captureIndex >= 2) {
-            captureIndex = 0;
-            captureDistance = (captureTime[1] - captureTime[0]) / (SystemCoreClock / 1000000) / 58;
+    if (TIMER_A0->CTL & TIMER_A_CTL_IFG) {
+        // Overflow interrupt triggered.
+        if (capture_index == 1) {
+            if (capture_offset == 0) {
+                capture_offset = UINT16_MAX_VALUE - capture_time[0];
+            } else {
+                capture_offset += UINT16_MAX_VALUE;
+            }
         }
 
-        TIMER_A0->CCTL[1] &= ~TIMER_A_CCTLN_COV;
+        TIMER_A0->CTL &= ~TIMER_A_CTL_IFG;
+    }
+
+    if (TIMER_A0->CCTL[1] & TIMER_A_CCTLN_CCIFG) {
+        // Rising or falling edge interrupt triggered.
+        if (capture_index == 0) {
+            // Rising edge interrupt.
+            capture_time[capture_index++] = TIMER_A0->CCR[1];
+        } else {
+            // Falling edge interrupt.
+            capture_time[capture_index] = TIMER_A0->CCR[1];
+
+            if ((TIMER_A0->CCTL[1] & TIMER_A_CCTLN_COV) == TIMER_A_CCTLN_COV) {
+                // Second interrupt happened but the software is too slow hence this data is corrupted and shall be ignored.
+                TIMER_A0->CCTL[1] &= ~TIMER_A_CCTLN_COV;
+            } else {
+                capture_distance = (capture_time[1] + capture_offset - capture_time[0]) / (SystemCoreClock / 1000000) / 58;
+            }
+        }
+
         TIMER_A0->CCTL[1] &= ~TIMER_A_CCTLN_CCIFG;
     }
 }
--- hc_sr04.h
+++ hc_sr04.h
@@ -10,17 +10,26 @@
  * Echo: Require Timer A capture input ports.
  */
 
-#define TRIGGER_PORT P4
-#define TRIGGER_PIN BIT3
+typedef struct {
+    volatile uint8_t *trigger_port_sel0;
+    volatile uint8_t *trigger_port_sel1;
+    volatile uint8_t *trigger_port_dir;
+    volatile uint8_t *trigger_port_out;
+    uint16_t trigger_port_pin;
 
-#define ECHO_PORT P2
-#define ECHO_PIN BIT4
+    volatile uint8_t *echo_port_sel0;
+    volatile uint8_t *echo_port_sel1;
+    volatile uint8_t *echo_port_dir;
+    uint16_t echo_port_pin;
+} hc_sr04_config;
 
-void setup_ultrasonic_pins(void);
-void setup_ultrasonic_capture_timers(void);
+void initialize_ultrasonic_sensor(const hc_sr04_config *config, const uint16_t threshold);
 void trigger_ultrasonic_sensor(void);
 
-void delay_us(int n);
+uint8_t is_object_found(void);
+uint16_t get_object_distance(void);
+
+void delay_us(const uint32_t amount);
 
 void TA0_N_IRQHandler(void);
 
